e706a976a40ff464c84da84e1318fd354adbb03f
Ökkes Erdem Köse <pl23ksc@eduvaud.ch>
2025-11-28 13:21:36 +0100

    feat(space-age) Exercice fini.

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, age) => {
+  const planets = 
+  {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  };
+
+  const earhYearsInSec = 31557600;
+  
+  const ageInYears = age/ earhYearsInSec / planets[planet];
+
+  if (!planets[planet]) 
+  {
+    throw new Error("not a planet");
+  }
+
+  return Math.round(ageInYears * 100)/ 100;
+
+
 };
