f1987fce00e92370c6ae45932ae13b506c9b3c96
Ökkes Erdem Köse <okkes.kose@eduvaud.ch>
2025-12-12 16:15:36 +0100

    Terminé

--- exercices/mixed-juices/mixed-juices.js
+++ exercices/mixed-juices/mixed-juices.js
@@ -11,10 +11,27 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-    const expr = name;
-    switch (expr) {
-      case ""
+    let number;
+    switch (name) {
+      case "Pure Strawberry Joy":
+        number= 0.5;
+        break;
+      case "Energizer":
+      case "Green Garden":
+        number= 1.5;
+        break;
+      case "Tropical Island":
+        number = 3;
+        break;
+      case "All or Nothing":
+        number = 5;
+        break;
+      default:
+        number = 2.5;
+        break;
+
     }
+    return number;
 
 }
 
@@ -26,8 +43,44 @@ export function timeToMixJuice(name) {
  * @param {string[]} limes
  * @returns {number} number of limes cut
  */
-export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Remove this line and implement the function');
+export function limesToCut(wedgesNeeded, limes) { 
+  let wedges = 0;
+  let limesCutted = 0;
+  let i=0;
+
+  // La boucle continue tant que le parametre wedgesNeeded et supérieur a wedge ET que i soit inférieur a la logneur du tab limes
+  while (wedges < wedgesNeeded && i < limes.length)
+  {
+    const lime = limes[i];
+
+    switch (lime) 
+    {
+      case "small":
+        wedges += 6;
+        break;
+
+      case "medium":
+        wedges += 8;
+        break;
+
+      case "large":
+        wedges += 10;
+        break;
+
+    }
+
+    limesCutted++;
+
+
+
+
+    i++;
+  }
+
+
+  return limesCutted;
+
+
 }
 
 /**
@@ -38,5 +91,20 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Remove this line and implement the function');
+  let i = 0;
+
+  // Continu tant que le temp est supérieure a 0 et i est inferieur a la longueur du tap orders
+  while (timeLeft > 0 && i < orders.length)
+  {
+    let order = orders[i];
+
+    timeLeft -= timeToMixJuice(order);
+
+    i++;
+
+  }
+
+  // Retourne un tableau coupé a l'index i puis garder le reste
+  return orders.slice(i);
+
 }
